1638086b4dd916225f609506766bf7f17d927774
Maxime Dor <max@kamax.io>
2015-03-21 10:57:18 +0100

    Updated About dialog

--- src/client/gui/default/src/org/altherian/hboxc/front/gui/AboutDialog.java
+++ src/client/gui/default/src/org/altherian/hboxc/front/gui/AboutDialog.java
@@ -2,19 +2,19 @@
  * Hyperbox - Enterprise Virtualization Manager
  * Copyright (C) 2013 Maxime Dor
  * hyperbox at altherian dot org
- * 
+ *
  * http://hyperbox.altherian.org
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -60,11 +60,11 @@ public class AboutDialog {
       text.setFont(UIManager.getFont("Label.font"));
 
       text.append("Hyperbox\n");
-      text.append("Copyright 2013 Maxime Dor & Katalin Dor. All rights reserved.\n");
+      text.append("Copyright 2013-2015 Kamax.io - All rights reserved.\n");
       text.append("This product is released under the GPL v3.\n");
       text.append("\n\n");
-      text.append("API Version " + HyperboxAPI.getVersion() + ")\n");
-      text.append("Client Version " + Hyperbox.getVersion() + ")\n");
+      text.append("API Version " + HyperboxAPI.getVersion() + "\n");
+      text.append("Client Version " + Hyperbox.getVersion() + "\n");
       text.append("Network Protocol Version " + HyperboxAPI.getProtocolVersion() + "\n");
       text.append("\n\n");
       text.append("Hyperbox is an Enterprise Virtualization Manager.\n");
@@ -73,7 +73,7 @@ public class AboutDialog {
       text.append("This software is made possible thanks to several open-source projects.\n");
       text.append("Refer to the User Manual for detailed names and licensing information.");
       text.append("\n\n");
-      text.append("Special thanks for their undying support and contribution to this project:\n");
+      text.append("Special thanks for their undying support and contributions to this project:\n");
       text.append("Perryg, klaus-vb");
 
       JPanel textPanel = new JPanel(new MigLayout("ins 50"));
