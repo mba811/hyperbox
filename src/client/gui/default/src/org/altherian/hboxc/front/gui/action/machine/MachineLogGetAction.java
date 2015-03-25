8cfcb7ce1ca4067680f4ff3a0001f2c07061e007
zzxyxy <zzxyxy@gmail.com>
2015-03-25 22:35:27 +0100

    Moved log viewer into its own class

--- src/client/gui/default/src/org/altherian/hboxc/front/gui/action/machine/MachineLogGetAction.java
+++ src/client/gui/default/src/org/altherian/hboxc/front/gui/action/machine/MachineLogGetAction.java
@@ -1,15 +1,11 @@
 package org.altherian.hboxc.front.gui.action.machine;
 
-import org.altherian.hbox.comm.HypervisorTasks;
-import org.altherian.hbox.hypervisor._MachineLogFile;
-import org.altherian.hboxc.front.gui.Gui;
-import org.altherian.hboxc.front.gui.builder.IconBuilder;
-import org.altherian.hboxc.front.gui.builder.JDialogBuilder;
+
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
-import javax.swing.JDialog;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import org.altherian.hbox.comm.HypervisorTasks;
+import org.altherian.hboxc.front.gui.builder.IconBuilder;
+import org.altherian.hboxc.front.gui.vm.MachineLogFileViewer;
 
 public class MachineLogGetAction extends AbstractAction {
 
@@ -29,20 +25,7 @@ public class MachineLogGetAction extends AbstractAction {
 
    @Override
    public void actionPerformed(ActionEvent ae) {
-      _MachineLogFile logIo = Gui.getServer(_srvId).getHypervisor().getLogFile(_vmId, "0");
-
-      JDialog dialog = JDialogBuilder.get("VM Log viewer");
-
-      JTextArea text = new JTextArea();
-      for (String line : logIo.getLog()) {
-         text.append(line + "\n");
-      }
-      dialog.getContentPane().add(new JScrollPane(text));
-
-      dialog.setSize(800, 600);
-      dialog.setLocationRelativeTo(dialog.getParent());
-
-      dialog.setVisible(true);
+      MachineLogFileViewer.show(_srvId, _vmId);
    }
 
 }
---
+++ src/client/gui/default/src/org/altherian/hboxc/front/gui/vm/MachineLogFileViewer.java
@@ -0,0 +1,63 @@
+/*
+ * Hyperbox - Enterprise Virtualization Manager
+ * Copyright (C) 2015 - Kamax.io
+ *
+ * http://hyperbox.altherian.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.altherian.hboxc.front.gui.vm;
+
+import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import org.altherian.hbox.hypervisor._MachineLogFile;
+import org.altherian.hboxc.front.gui.Gui;
+import org.altherian.hboxc.front.gui.builder.JDialogBuilder;
+
+public class MachineLogFileViewer {
+
+   private String _srvId;
+   private String _vmId;
+
+   private JDialog dialog;
+
+   public MachineLogFileViewer(String srvId, String vmId) {
+      _srvId = srvId;
+      _vmId = vmId;
+
+      _MachineLogFile logIo = Gui.getServer(_srvId).getHypervisor().getLogFile(_vmId, "0");
+
+      JDialog dialog = JDialogBuilder.get("VM Log viewer");
+      JTextArea text = new JTextArea();
+      for (String line : logIo.getLog()) {
+         text.append(line + "\n");
+      }
+
+      dialog.getContentPane().add(new JScrollPane(text));
+      dialog.setSize(800, 600);
+      dialog.setLocationRelativeTo(dialog.getParent());
+   }
+
+   public void show() {
+      dialog.setVisible(true);
+   }
+
+   public static void show(String srvId, String vmId) {
+      (new MachineLogFileViewer(srvId, vmId)).show();
+   }
+
+}
